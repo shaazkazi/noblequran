e23486cd0c3f037ec4756dcdb2ae1283e8751a00
shaazkazi <n3tn1nj4@proton.me>
2025-02-19 14:08:36 +0300

    Update BottomTabs.jsx

--- src/components/Navigation/BottomTabs.jsx
+++ src/components/Navigation/BottomTabs.jsx
@@ -6,17 +6,17 @@ const BottomTabs = () => {
   return (
     <nav className="ios-bottom-tabs">
       <Link to="/" className={`flex flex-col items-center ${location.pathname === '/' ? 'text-ios-primary' : 'text-gray-600'}`}>
-        <img src="/surahs.svg" alt="Surahs" className="w-6 h-6" />
+        <img src="/surahs.svg" alt="Surahs" className="w-6 h-6 tab-icon" />
         <span className="text-xs mt-1">Surahs</span>
       </Link>
 
       <Link to="/reciters" className={`flex flex-col items-center ${location.pathname === '/reciters' ? 'text-ios-primary' : 'text-gray-600'}`}>
-        <img src="/mic.svg" alt="Reciters" className="w-6 h-6" />
+        <img src="/mic.svg" alt="Reciters" className="w-6 h-6 tab-icon" />
         <span className="text-xs mt-1">Reciters</span>
       </Link>
 
       <Link to="/settings" className={`flex flex-col items-center ${location.pathname === '/settings' ? 'text-ios-primary' : 'text-gray-600'}`}>
-        <img src="/settings.svg" alt="Settings" className="w-6 h-6" />
+        <img src="/settings.svg" alt="Settings" className="w-6 h-6 tab-icon" />
         <span className="text-xs mt-1">Settings</span>
       </Link>
     </nav>
@@ -24,4 +24,3 @@ const BottomTabs = () => {
 };
 
 export default BottomTabs;
-
