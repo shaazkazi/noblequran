511bf25361b0235de936c87bb8213fae3737e7d9
shaazkazi <n3tn1nj4@proton.me>
2025-02-19 13:37:35 +0300

    Update BottomTabs.jsx

--- src/components/Navigation/BottomTabs.jsx
+++ src/components/Navigation/BottomTabs.jsx
@@ -1,24 +1,26 @@
 import { Link, useLocation } from 'react-router-dom';
-import { BookOpenIcon, MicrophoneIcon, CogIcon } from '@heroicons/react/24/outline';
+import surahsIcon from '../assets/surahs.svg';
+import micIcon from '../assets/mic.svg';
+import settingsIcon from '../assets/settings.svg';
 
 const BottomTabs = () => {
   const location = useLocation();
 
   return (
     <nav className="ios-bottom-tabs">
-      <Link to="/" className={`flex flex-col items-center ${location.pathname === '/' ? 'text-ios-primary' : 'text-gray-600'}`}>
-        <BookOpenIcon className="w-6 h-6" />
-        <span className="text-xs mt-1">Surahs</span>
+      <Link to="/" className={`tab-item ${location.pathname === '/' ? 'active' : ''}`}>
+        <img src={surahsIcon} alt="Surahs" className="tab-icon" />
+        <span>Surahs</span>
       </Link>
       
-      <Link to="/reciters" className={`flex flex-col items-center ${location.pathname === '/reciters' ? 'text-ios-primary' : 'text-gray-600'}`}>
-        <MicrophoneIcon className="w-6 h-6" />
-        <span className="text-xs mt-1">Reciters</span>
+      <Link to="/reciters" className={`tab-item ${location.pathname === '/reciters' ? 'active' : ''}`}>
+        <img src={micIcon} alt="Reciters" className="tab-icon" />
+        <span>Reciters</span>
       </Link>
       
-      <Link to="/settings" className={`flex flex-col items-center ${location.pathname === '/settings' ? 'text-ios-primary' : 'text-gray-600'}`}>
-        <CogIcon className="w-6 h-6" />
-        <span className="text-xs mt-1">Settings</span>
+      <Link to="/settings" className={`tab-item ${location.pathname === '/settings' ? 'active' : ''}`}>
+        <img src={settingsIcon} alt="Settings" className="tab-icon" />
+        <span>Settings</span>
       </Link>
     </nav>
   );
